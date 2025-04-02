81224df0082198c9a8c112bab3c04460b21d7f8b
fnisthal <fnisthal@gmail.com>
2025-04-02 11:58:18 -0600

    Regresar los archivos a su estado original

--- models/res_company.py
+++ models/res_company.py
@@ -7,16 +7,9 @@ from odoo.release import version_info
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    nombre_comercial = fields.Char('Nombre Comercial', copy=False)
     certificador_fel = fields.Selection([], 'Certificador FEL')
     afiliacion_iva_fel = fields.Selection([('GEN', 'GEN'), ('PEQ', 'PEQ'), ('EXE', 'EXE')], 'Afiliación IVA FEL', default='GEN')
     tipo_personeria_fel = fields.Char('Tipo Personeria FEL')
     frases_fel = fields.Text('Frases FEL')
     adenda_fel = fields.Text('Adenda FEL')
 
-# 🔒 Restricción para validar que el nombre comercial esté definido
-    @api.constrains('nombre_comercial')
-    def _check_nombre_comercial_fel(self):
-        for rec in self:
-            if not rec.nombre_comercial:
-                raise ValidationError("Debe ingresar el campo 'Nombre Comercial' para emitir facturas electrónicas (FEL).")
