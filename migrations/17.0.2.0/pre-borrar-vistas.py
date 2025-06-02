8570d7f38ca684c05513a0e7132e7315f46f26ba
rocketgithub <rodrigo@aquih.com>
2025-06-02 15:06:32 -0600

    Mejor utilizar las utilidades de Odoo

--- migrations/17.0.2.0/pre-borrar-vistas.py
+++ migrations/17.0.2.0/pre-borrar-vistas.py
@@ -1,8 +1,13 @@
 import logging
+from odoo.upgrade import util
 
 _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    cr.execute("delete from ir_model_data where module = 'fel_gt' and model = 'ir.ui.view' and name like '%view%';")
-    _logger.info("Referencias a vistas viejas borradas")
+    util.records.remove_view(cr, xml_id="fel_gt.invoice_form_fel_gt")
+    util.records.remove_view(cr, xml_id="fel_gt.journal_form_fel_gt")
+    util.records.remove_view(cr, xml_id="fel_gt.view_tax_form_fel_gt")
+    util.records.remove_view(cr, xml_id="fel_gt.view_partner_form_fel_gt")
+    util.records.remove_view(cr, xml_id="fel_gt.view_company_form_fel_gt")
+    _logger.info("Vistas viejas borradas")
