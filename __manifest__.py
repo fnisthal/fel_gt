773a17dcff8d6da70bc471d7f3c55537d0abc75f
rocketgithub <rodrigo@aquih.com>
2025-10-22 10:53:29 -0600

    Mejor descripción de campo.

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Guatemala',
-    'version': '1.7',
+    'version': '1.8',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
