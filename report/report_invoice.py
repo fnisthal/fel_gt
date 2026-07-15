38774c412b43a5e76143b545f781c51e45d91d64
TutecGT <fnisthal@tutec.gt>
2026-07-15 09:19:45 -0600

    Evitar modificar contingencia FEL al imprimir

--- report/report_invoice.py
+++ report/report_invoice.py
@@ -5,10 +5,12 @@ class ReportAccountReport_Invoice(models.AbstractModel):
     _inherit = 'report.account.report_invoice'
 
     def _get_report_values(self, docids, data=None):
-        docs = self.env['account.move'].browse(docids)
-        for invoice in docs:
-            if not invoice.firma_fel:
-                invoice.contingencia_fel = True
-            else:
-                invoice.contingencia_fel = False
+        # La generación del reporte no debe modificar el estado de contingencia FEL.
+        # Se conserva la lógica anterior comentada como referencia:
+        # docs = self.env['account.move'].browse(docids)
+        # for invoice in docs:
+        #     if not invoice.firma_fel:
+        #         invoice.contingencia_fel = True
+        #     else:
+        #         invoice.contingencia_fel = False
         return super()._get_report_values(docids, data)
