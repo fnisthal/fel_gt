2542f4aafdbe71177219bebebb1d0179f70d5aa1
fnisthal <fnisthal@gmail.com>
2026-08-15 19:14:33 -0600

    Corrige selectores de ribbons en vista FEL

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Guatemala',
-    'version': '19.0.1.16.4',
+    'version': '19.0.1.16.5',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
